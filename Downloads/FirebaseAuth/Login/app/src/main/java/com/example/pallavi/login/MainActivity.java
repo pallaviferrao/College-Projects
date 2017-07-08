68a2ab97e05289f046ed449b65bf952aa5425820
pallaviferrao <pallaviferrao2004@gmail.com>
2017-07-08 17:13:10 +0530

    a

--- Downloads/FirebaseAuth/Login/app/src/main/java/com/example/pallavi/login/MainActivity.java
+++ Downloads/FirebaseAuth/Login/app/src/main/java/com/example/pallavi/login/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         progressDialog.dismiss();
                     }
                 });
-o
+
     }
 
     @Override
